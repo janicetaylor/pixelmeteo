0eae6dc84bb08ce4ab8a70b132f3e34590afa60b
Janice Taylor <notjanicetaylor@gmail.com>
2024-04-11 15:04:24 -0500

    fixing build errors

--- PixelMeteo/PixelMeteo/PixelMeteoApp.swift
+++ PixelMeteo/PixelMeteo/PixelMeteoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PixelMeteoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(weatherViewModel: WeatherViewModel())
         }
     }
 }
